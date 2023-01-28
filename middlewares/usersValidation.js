555dac9548ebcb6c88a275ffd7dd9aac6b5ab16b
kamilawrzesniewska <kamila.wrzesniewska07@gmail.com>
2023-01-28 19:41:35 +0100

    middl

---
+++ middlewares/usersValidation.js
@@ -0,0 +1,37 @@
+const Joi = require("joi");
+
+const schemafindUserByEmail = Joi.object({
+	email: Joi.string()
+		.email({
+			minDomainSegments: 2,
+			tlds: { allow: ["com", "net", "pl"] },
+		})
+		.required(),
+	password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).required(),
+});
+
+const schemaPatchSubscription = Joi.object({
+	subscription: Joi.string().valid("starter", "pro", "business").required(),
+});
+
+const validate = (schema, obj, next, res) => {
+	const { error } = schema.validate(obj);
+	if (error) {
+		const [{ message }] = error.details;
+		console.log(error);
+		return res.json({
+			status: "failure",
+			code: 400,
+			message: `Field ${message.replace(/"/g, "")}`,
+		});
+	}
+	next();
+};
+
+module.exports.findUserByEmail = (req, res, next) => {
+	return validate(schemafindUserByEmail, req.body, next, res);
+};
+
+module.exports.patchSubscription = (req, res, next) => {
+	return validate(schemaPatchSubscription, req.body, next, res);
+};
