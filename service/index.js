0c244fc7b6e8cb622c35e042b9c38ac91ed8ca2b
kamilawrzesniewska <kamila.wrzesniewska07@gmail.com>
2023-02-10 22:21:17 +0100

    t2

--- service/index.js
+++ service/index.js
@@ -37,7 +37,25 @@ const deleteContact = async (contactId) => {
 	return Contact.deleteOne({ _id: objectIdContactId });
 };
 
-
+const updateContact = async (contactId, body) => {
+	let objectIdContactId;
+	try {
+		objectIdContactId = ObjectId(contactId);
+	} catch (error) {
+		return null;
+	}
+	return Contact.findOneAndUpdate(
+		{
+			_id: objectIdContactId,
+		},
+		{ $set: body },
+		{
+			new: true,
+			runValidators: true,
+			strict: "throw",
+		}
+	);
+};
 
 module.exports = {
 	getAllContacts,
