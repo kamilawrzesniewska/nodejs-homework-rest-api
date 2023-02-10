c0969e0f5c416ed996df0266410ba6eedae7d25e
kamilawrzesniewska <kamila.wrzesniewska07@gmail.com>
2023-02-10 22:11:21 +0100

    kl

--- routes/contacts.js
+++
@@ -1,8 +0,0 @@
-router.get("/", authMiddleware, contactController.get);
-router.get("/:contactId", contactController.getOne);
-router.post(
-	"/",
-	authMiddleware,
-	validate.createContact,
-	contactController.post
-);
