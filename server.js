e7b3fce47ccf44b6c3a892e109de3625f1375524
kamilawrzesniewska <kamila.wrzesniewska07@gmail.com>
2023-02-10 22:31:49 +0100

    update

--- server.js
+++ server.js
@@ -1,42 +1,6 @@
-const express = require("express");
-const logger = require("morgan");
-const cors = require("cors");
 const mongoose = require("mongoose");
+const app = require("./app");
 
-require("dotenv").config();
-
-const indexRouter = require("./routes/api/index");
-
-const app = express();
-
-const formatsLogger = app.get("env") === "development" ? "dev" : "short";
-
-app.use(logger(formatsLogger));
-app.use(cors());
-app.use(express.json());
-
-require("./config/passport");
-
-app.use("/api", indexRouter);
-
-
-app.use((req, res) => {
-	res.status(404).json({
-		status: "error",
-		code: 404,
-		message: "Use api on routes: /api/contacts",
-		data: "Not found",
-	});
-});
-
-app.use((err, req, res, next) => {
-	res.status(500).json({
-		status: "fail",
-		code: 500,
-		message: err.message,
-		data: "Internal Server Error",
-	});
-});
 
 const uriDb = process.env.DB_URI;
 const PORT = process.env.PORT || 3000;
