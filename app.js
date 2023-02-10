7dfce69bddd7803720b84b22dd74628615f6784c
kamilawrzesniewska <kamila.wrzesniewska07@gmail.com>
2023-02-10 22:25:37 +0100

    app

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+require("dotenv").config();
+const indexRouter = require("./routes/api/index");
+const app = express();
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+require("./config/passport");
+
+app.use("/api", indexRouter);
+
+app.use((req, res) => {
+	res.status(404).json({
+		status: "error",
+		code: 404,
+		message: "Use api on routes: /api/contacts",
+		data: "Not found",
+	});
+});
+
+app.use((err, req, res, next) => {
+	res.status(500).json({
+		status: "fail",
+		code: 500,
+		message: err.message,
+		data: "Internal Server Error",
+	});
+});
+
+module.exports = app;
