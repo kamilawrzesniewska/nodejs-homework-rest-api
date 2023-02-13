1a7e1f74cdb0e2da1da6a4f66938a542b883ab90
kamilawrzesniewska <kamila.wrzesniewska07@gmail.com>
2023-02-13 20:40:44 +0100

    d

--- routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.use("/contacts", require("./contacts"));
-router.use("/users", require("./auth"));
-
-module.exports = router;
