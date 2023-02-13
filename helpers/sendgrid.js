2ba02abd197f6b96e4c3b1ec3912130bf194dbac
kamilawrzesniewska <kamila.wrzesniewska07@gmail.com>
2023-02-13 20:30:06 +0100

    mailbody

--- helpers/sendgrid.js
+++ helpers/sendgrid.js
@@ -1,3 +1,31 @@
 const sgMail = require("@sendgrid/mail");
 const Mailgen = require("mailgen");
 require("dotenv").config();
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const mailGenerator = new Mailgen({
+	theme: "default",
+	product: {
+		name: "Contacts App",
+		link: "http://localhost:8000/api/users/login",
+	},
+});
+
+const emailBody = (email, verificationToken) => {
+	return {
+		body: {
+			name: `${email}`,
+			intro:
+				"You have received this email because you need to verify your account.",
+			action: {
+				instructions: "Click the button below to verify your email:",
+				button: {
+					color: "#454545",
+                    text: "Verify your email",
+					link: `http://localhost:8000/api/users/verify/${verificationToken}`,
+				},
+			},
+		},
+	};
+};
